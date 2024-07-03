2223263769d1f3b78365f35956f80001d8dd1f2e
lhj1101 <751609117@qq.com>
2024-07-04 01:57:27 +0800

    feat：修改全局样式

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
-  <router-view></router-view>
+  <div>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script setup lang="ts">
 
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,24 @@
+<template>
+  <div class="hello">
+    123
+    <div>
+      {{ store.count }}
+    </div>
+    <div>
+      {{store.double  }}
+    </div>
+    <div>
+      <button @click="store.increment">按钮点击调用increment方法</button>
+    </div>
+
+    <div class="l-size content">
+      123456
+    </div>
+  </div>
+</template>
+
 <script setup lang="ts">
 import { ref } from 'vue'
-
 import { useCounterStore } from '@/store'
 // 可以在组件中的任意位置访问 `store` 变量 ✨
 const store = useCounterStore()
@@ -10,27 +28,12 @@ console.log(store.double); // double  10*2=20
 
 </script>
 
-<template>
-  123
-  <div>
-    {{ store.count }}
-  </div>
-  <div>
-    {{store.double  }}
-  </div>
-  <div>
-    <button @click="store.increment">按钮点击调用increment方法</button>
-  </div>
-
-  <div class="l-size content">
-    123456
-  </div>
-
-
-</template>
 
 <style scoped lang="scss">
 .content{
   color: $color-red;
+}
+.hello{
+  // background-color: $color-red;
 }
 </style>
