a24ff8caf4725c0093f29e57cb5621219667b06c
lhj1101 <751609117@qq.com>
2025-01-12 15:03:18 +0800

    feat: 定义全局配置

--- src/utils/storage/lStorageConfig.js
+++ src/utils/storage/lStorageConfig.js
@@ -1,7 +1,7 @@
 export const lStorageConfig = {
   state: {
-    name: 'happy-', // 系统前缀
-    expireTimeList: 'EXPIRE-TIME-LIST', // ls过期时间合集
+    name: import.meta.env.LS_STORAGE_KEY || 'SYSTEM-', // 系统前缀
+    expireTimeList: import.meta.env.LS_STORAGE_KEY + 'EXPIRE-TIME-LIST', // ls过期时间合集
     time: 86400, // 单位秒，默认一天
   },
   // 需要配置key才能setls
